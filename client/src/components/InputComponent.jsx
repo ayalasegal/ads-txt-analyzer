a8300c94e13b69668e4c03255ea3c18e2e781790
ayalasegal <ayalasegal1@gmail.com>
2023-11-26 22:17:54 +0200

    add key press feature to button

--- client/src/components/InputComponent.jsx
+++ client/src/components/InputComponent.jsx
@@ -1,25 +1,30 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
 
 function InputComponent({ onDomainDetails }) {
-  const [domain, setDomain] = useState('');
+  const [domain, setDomain] = useState("");
 
   const handleClick = () => {
     onDomainDetails(domain);
   };
-
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleClick();
+    }
+  };
   return (
     <div className="input-container">
       <TextField
-        className='input-text-field'
+        className="input-text-field"
         placeholder="Enter domain name... (e.g. msn.com)"
         variant="outlined"
         onChange={(e) => setDomain(e.target.value)}
+        onKeyPress={handleKeyPress}
       />
       <Button
         variant="contained"
-        className='input-button'
+        className="input-button"
         onClick={handleClick}
       >
         Parse Ads.txt
