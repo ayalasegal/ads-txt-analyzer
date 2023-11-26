80830a017960643422d7429249c2592e5a6a5c59
ayalasegal <ayalasegal1@gmail.com>
2023-11-26 15:12:49 +0200

    change back to requiring

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,16 @@
-import express from "express";
-import cors from "cors";
-import { json } from "body-parser";
-import adsRouter from "./routes/adsRouter";
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const adsRouter = require("./routes/adsRouter");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
-app.use(json());
+app.use(bodyParser.json());
 app.use("/", adsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-export default app; // Export the app for testing purposes
+module.exports = app; // Export the app for testing purposes
