b5eda2d6a5336e03cea6e66c59c68b51ab5f788f
ayalasegal <ayalasegal1@gmail.com>
2023-11-26 20:43:37 +0200

    modify to avoid Not Found Error

--- server/src/app.js
+++ server/src/app.js
@@ -9,13 +9,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Catch-all route to serve the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use("/", adsRouter);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
