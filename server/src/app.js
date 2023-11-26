374e0e4e205f28085f2afcb859846c3a31fa71ad
ayalasegal <ayalasegal1@gmail.com>
2023-11-26 16:36:10 +0200

    fix Not Found

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/", adsRouter);
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
