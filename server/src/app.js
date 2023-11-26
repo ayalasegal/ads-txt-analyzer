657175f1b9cfbe60e815e16cbabafc77f4903640
ayalasegal <ayalasegal1@gmail.com>
2023-11-26 20:56:14 +0200

    modify patch to the right route

--- server/src/app.js
+++ server/src/app.js
@@ -9,11 +9,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Catch-all route to serve the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 app.use("/", adsRouter);
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
